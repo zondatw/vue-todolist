43dc9233ac7384acf3735e7b89fbbf8b872f8c22
zondaTW <u226699@gmail.com>
2020-02-07 14:46:18 +0800

    Add scss of todo title overflow

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,33 @@
             </div>
             <!-- title -->
             <div class="todo-title">
+              <span>test yooooooooo000000000000</span>
+            </div>
+            <!-- star and edit button -->
+            <div class="todo-control">
+              <i class="far fa-star"></i>
+              <i class="fas fa-pencil-alt"></i>
+            </div>
+          </div>
+          <div class="todo-body">
+
+          </div>
+          <div class="todo-footer text-secondary">
+            <!-- date and icon of file and message -->
+            <i class="far fa-calendar-alt todo-icon"><span>1991/01/01</span></i>
+            <i class="far fa-file todo-icon"></i>
+            <i class="far fa-comment-dots todo-icon"></i>
+          </div>
+        </div>
+        <div class="todo-item">
+          <div class="todo-header">
+            <!-- checkbox -->
+            <div class="todo-check">
+              <input type="checkbox" id="checkboxTest2">
+              <label for="checkboxTest2"></label>
+            </div>
+            <!-- title -->
+            <div class="todo-title">
               <span>test</span>
             </div>
             <!-- star and edit button -->
@@ -32,7 +59,7 @@
           </div>
           <div class="todo-footer text-secondary">
             <!-- date and icon of file and message -->
-            <i class="far fa-calendar-alt todo-icon"></i>
+            <i class="far fa-calendar-alt todo-icon"><span>1991/01/01</span></i>
             <i class="far fa-file todo-icon"></i>
             <i class="far fa-comment-dots todo-icon"></i>
           </div>

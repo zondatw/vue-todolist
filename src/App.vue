208bdd8995ece993bc93a1f0096db4ec59b2b063
zondaTW <u226699@gmail.com>
2020-02-07 11:43:38 +0800

    Add simple template of todo item

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,38 @@
         <a href="#" :class="{active: currentTab === 'completed'}" @click="currentTab = 'completed'">Completed</a>
       </div>
     </div>
-    
 
+    <div class="container">
+      <div class="mt-4">
+        <div class="todo-item">
+          <div class="todo-header">
+            <!-- checkbox -->
+            <div class="todo-check">
+              <input type="checkbox">
+              <label></label>
+            </div>
+            <!-- title -->
+            <div class="todo-title">
+              <span>test</span>
+            </div>
+            <!-- star and edit button -->
+            <div class="todo-control">
+              <i class="far fa-star"></i>
+              <i class="fas fa-pencil-alt"></i>
+            </div>
+          </div>
+          <div class="todo-body">
+
+          </div>
+          <div class="todo-footer">
+            <!-- date and icon of file and message -->
+            <i class="far fa-calendar-alt"></i>
+            <i class="far fa-file"></i>
+            <i class="far fa-comment-dots"></i>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 

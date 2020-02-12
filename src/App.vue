924ddf88ce59629599457f8cf3463fb15dec6554
zondaTW <u226699@gmail.com>
2020-02-12 15:00:38 +0800

    Add todo edit block

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,84 @@
           <todo-item
             :todo="item"
           ></todo-item>
+        <div class="todo-item">
+          <div class="todo-header">
+            <!-- completed checkbox -->
+            <div class="todo-check">
+              <input
+                type="checkbox" :id="`todo-checkbox-`"
+              >
+              <label :for="`todo-checkbox-`"></label>
+            </div>
+            <!-- todo title -->
+            <div class="todo-title">
+              <input type="text" class="form-control">
+            </div>
+            <!-- star and editing button -->
+            <div class="todo-control">
+              <i class="fas fa-star stared"></i>
+              <i class="fas fa-pencil-alt editing"></i>
+            </div>
+          </div>
+          <div class="todo-body">
+            <div class="todo-container">
+              <!-- deadline -->
+              <div class="todo-row">
+                <div class="todo-row-icon">
+                  <i class="far fa-calendar-alt"></i>
+                </div>
+                <div class="todo-row-body">
+                  <b>Deadline</b>
+                  <div class="form-inline todo-form-control">
+                    <input type="date" class="form-control border-0">
+                    <input type="time" class="form-control border-0 ml-2">
+                  </div>
+                </div>
+              </div>
+              <!-- file -->
+              <div class="todo-row">
+                <div class="todo-row-icon">
+                  <i class="far fa-file"></i>
+                </div>
+                <div class="todo-row-body">
+                  <b>File</b>
+                  <div class="form-inline todo-form-control todo-file">
+                    <div class="todo-file-col">
+                      <input :id="`todo-edit-file`" type="file">
+                      <label :for="`todo-edit-file`"></label>
+                    </div>
+                    <div class="todo-file-col">
+                      <label :id="`todo-edit-file-content`" class="todo-file-content"></label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- comment -->
+              <div class="todo-row">
+                <div class="todo-row-icon">
+                  <i class="far fa-calendar-alt fa-comment-dots"></i>
+                </div>
+                <div class="todo-row-body">
+                  <b>Comment</b>
+                  <div class="todo-form-control">
+                    <textarea
+                      class="form-control border-0"
+                      placeholder="Type your memo here..."
+                    ></textarea>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="d-flex">
+            <!-- cancel and add task button -->
+            <button class="btn btn-light text-danger w-50 btn-lg rounded-0">
+              <i class="fas fa-times"></i> Cancel
+            </button>
+            <button class="btn btn-primary w-50 btn-lg rounded-0">
+              <i class="fas fa-plus"></i> Update Task
+            </button>
+          </div>
         </div>
       </div>
       <div class="mt-4">
@@ -65,6 +143,15 @@ export default {
     TodoItem,
     draggable
   }
+}
+
+window.onload = function () {
+  var todoEditFile = document.getElementById('todo-edit-file')
+  todoEditFile.addEventListener('change', function(e) {
+    let fileName = e.target.value.split('\\').pop().split('/').pop();
+    let todoEditFileContent = document.getElementById('todo-edit-file-content');
+    todoEditFileContent.innerText = fileName;
+  })
 }
 </script>
 

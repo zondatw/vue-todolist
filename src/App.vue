ae1f4a1ccc183699e6d1668dd16c2d4e2732bf21
zondaTW <u226699@gmail.com>
2020-02-12 15:29:08 +0800

    Add new component: EditTodoItem

--- src/App.vue
+++ src/App.vue
@@ -10,89 +10,21 @@
 
     <div class="container">
       <div class="mt-4">
-        <div v-for="item in todos" :key="item.id">
-          <todo-item
-            :todo="item"
-          ></todo-item>
-        <div class="todo-item">
-          <div class="todo-header">
-            <!-- completed checkbox -->
-            <div class="todo-check">
-              <input
-                type="checkbox" :id="`todo-checkbox-`"
-              >
-              <label :for="`todo-checkbox-`"></label>
-            </div>
-            <!-- todo title -->
-            <div class="todo-title">
-              <input type="text" class="form-control">
-            </div>
-            <!-- star and editing button -->
-            <div class="todo-control">
-              <i class="fas fa-star stared"></i>
-              <i class="fas fa-pencil-alt editing"></i>
-            </div>
-          </div>
-          <div class="todo-body">
-            <div class="todo-container">
-              <!-- deadline -->
-              <div class="todo-row">
-                <div class="todo-row-icon">
-                  <i class="far fa-calendar-alt"></i>
-                </div>
-                <div class="todo-row-body">
-                  <b>Deadline</b>
-                  <div class="form-inline todo-form-control">
-                    <input type="date" class="form-control border-0">
-                    <input type="time" class="form-control border-0 ml-2">
-                  </div>
-                </div>
-              </div>
-              <!-- file -->
-              <div class="todo-row">
-                <div class="todo-row-icon">
-                  <i class="far fa-file"></i>
-                </div>
-                <div class="todo-row-body">
-                  <b>File</b>
-                  <div class="form-inline todo-form-control todo-file">
-                    <div class="todo-file-col">
-                      <input :id="`todo-edit-file`" type="file">
-                      <label :for="`todo-edit-file`"></label>
-                    </div>
-                    <div class="todo-file-col">
-                      <label :id="`todo-edit-file-content`" class="todo-file-content"></label>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <!-- comment -->
-              <div class="todo-row">
-                <div class="todo-row-icon">
-                  <i class="far fa-calendar-alt fa-comment-dots"></i>
-                </div>
-                <div class="todo-row-body">
-                  <b>Comment</b>
-                  <div class="todo-form-control">
-                    <textarea
-                      class="form-control border-0"
-                      placeholder="Type your memo here..."
-                    ></textarea>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="d-flex">
-            <!-- cancel and add task button -->
-            <button class="btn btn-light text-danger w-50 btn-lg rounded-0">
-              <i class="fas fa-times"></i> Cancel
-            </button>
-            <button class="btn btn-primary w-50 btn-lg rounded-0">
-              <i class="fas fa-plus"></i> Update Task
-            </button>
-          </div>
+        <div
+          v-if="!isNewTodo"
+          class="todo-add"
+        >
+          <i class="fa fa-plus"></i>
+          <input
+            type="text"
+            class="form-control form-control-lg"
+            @focus="addTodo"
+          >
         </div>
+        <edit-todo-item
+          v-if="isNewTodo"
+          @closeEditTodo="closeEdit"
+        ></edit-todo-item>
       </div>
       <div class="mt-4">
         <draggable v-model="todos">
@@ -109,6 +41,7 @@
 
 <script>
 import TodoItem from './components/TodoItem'
+import EditTodoItem from './components/EditTodoItem'
 import draggable from 'vuedraggable'
 
 export default {
@@ -116,6 +49,7 @@ export default {
   data () {
     return {
       currentTab: 'myTasks',
+      isNewTodo: false,
       todos: [
         {
           'id': 1,
@@ -134,28 +68,47 @@ export default {
           'stared': false,
           'comment': `I'm test test test test`,
           'completed': false,
-
+        },
+        {
+          'id': 3,
+          'title': 'test 123456',
+          'stared': false,
+          'comment': `I'm test test test test`,
+          'completed': false,
         },
       ],
     }
   },
   components: {
     TodoItem,
+    EditTodoItem,
     draggable
+  },
+  methods: {
+    addTodo () {
+      this.isNewTodo = true;
+    },
+    closeEdit () {
+      this.isNewTodo = false;
+    }
   }
-}
-
-window.onload = function () {
-  var todoEditFile = document.getElementById('todo-edit-file')
-  todoEditFile.addEventListener('change', function(e) {
-    let fileName = e.target.value.split('\\').pop().split('/').pop();
-    let todoEditFileContent = document.getElementById('todo-edit-file-content');
-    todoEditFileContent.innerText = fileName;
-  })
 }
 </script>
 
 <style lang="scss">
+.todo-add {
+  display: flex;
+  position: relative;
+
+  i {
+    color: #9B9B9B;
+    font-size: 1.5rem;
+    position: absolute;
+    left: 1rem;
+    top: 0.75rem;
+  }
+}
+
 .draggable {
   cursor: move;
 }

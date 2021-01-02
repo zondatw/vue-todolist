b2d7012191889d0e06038d9ff10632cf0c9c4612
zonda yang <u226699@gmail.com>
2021-01-03 00:38:25 +0800

    Add new page: register

---
+++ src/pages/Register.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="register" class="container">
+    <div class="row">
+      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card card-signin my-5">
+          <div class="card-body">
+            <h5 class="card-title text-center">Sign In</h5>
+            <form class="form-signin" @submit.prevent="login">
+              <div class="form-label-group">
+                <label for="inputUsername">Username</label>
+                <input
+                  type="username"
+                  id="inputUsername"
+                  class="form-control"
+                  placeholder="Username"
+                  v-model="registerData.username"
+                  required
+                  autofocus
+                >
+              </div>
+              <div class="form-label-group">
+                <label for="inputPassword">Password</label>
+                <input
+                  type="password"
+                  id="inputPassword"
+                  class="form-control"
+                  placeholder="Password"
+                  v-model="registerData.password"
+                  required
+                >
+              </div>
+              <hr class="my-4">
+              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign up</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'register',
+  data () {
+    return {
+      registerData: {
+        username: '',
+        password: '',
+      }
+    }
+  },
+  methods: {
+    register () {
+      const vm = this
+      const api = `register`
+      const tempRegisterData = {...this.registerData}
+
+      vm.$http
+        .post(api, tempRegisterData)
+        .then(
+          response => {
+            this.$router.push('/login')
+            return response
+          }
+        )
+        .catch(
+          () => {
+            alert('register failed')
+          }
+        )
+    }
+  }
+}
+</script>
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import Login from './pages/Login.vue'
+import Register from './pages/Register.vue'
 import Home from './pages/Home.vue'
 
 export const routes = [
   {
     path: '/login',
     component: Login
+  },
+  {
+    path: '/register',
+    component: Register
   },
   {
     path: '/',
